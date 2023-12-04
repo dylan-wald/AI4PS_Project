50cc70d01a7c5d9b6f7d2e470495a9452faf93cd
dylan-wald <54458575+dylan-wald@users.noreply.github.com>
2023-12-04 07:48:32 -0700

    Optuna study

--- main.py
+++ main.py
@@ -16,7 +16,8 @@ torch.manual_seed(101)
 
 ### data file
 # file = "data/combinedDataRev3Labeled.csv"
-file = "data/combinedDataRev4SlimLabeled.csv"
+# file = "data/combinedDataRev4SlimLabeled.csv"
+file = "data/combinedDataRev4LabeledSynthetic.csv"
 
 ### create dataframe
 DataObj = Data(file)
@@ -26,10 +27,10 @@ df = DataObj.ImportData()
 Vc_colnames = ["vc1","vc2","vc3","vc4","vc5","vc6","vc7","vc8"]
 X_colnames = ["g1upper","g2upper","g3upper", "g4upper","g5upper","g6upper","g7upper","g8upper","i1","i2"]
 y_colnames = ["vout"]
-X = np.array(df[X_colnames])[:10000,:]
-y = np.array(df[y_colnames])[1:10001,:]
+X = np.array(df[X_colnames])[5000:15000,:]
+y = np.array(df[y_colnames])[5000:15000,:]
 
-Vc_all = np.array(df[Vc_colnames])[:10000,:]
+Vc_all = np.array(df[Vc_colnames])[5000:15000,:]
 
 # ### normalize the training data
 # norm = StandardScaler().fit(X)
@@ -50,32 +51,32 @@ train_data = CustomDataset(X_train, y_train)
 test_data = CustomDataset(X_test, y_test)
 
 ### use the generated train/test data and batch size to create DataLoader objects for the train and test (validataion) sets
-batch_size = 10
+batch_size = 200
 trainloader = DataLoader(train_data, batch_size=batch_size, shuffle=False)
 valloader = DataLoader(test_data, batch_size=batch_size, shuffle=False)
 
 # ### neural network parameters
 in_dim = 10
-hidden_dim = 128
+hidden_dim = 201 #137
 out_dim = 9
 
 ### define the neural network
 model = LSTM(in_dim, out_dim, hidden_dim)
 
 ### training parameters
-num_epochs = 50
-learn_rate = 0.00001
+num_epochs = 500
+learn_rate = 0.007026062472237584 #0.019861825111735603 #0.00001
 
 ### Define the loss function
 physics_loss_func = PINNLoss()
 loss_func = torch.nn.MSELoss()
 
 # weight on nn output to true output loss
-gamma1 = .1 #1.0 / 47260.1495314765 #5e-6
+gamma1 = 2.2642158726325947 #4.7670423013652234 #.1 
 # weight on nn state output to dynamic state output loss
-gamma2 = 10. #1e0 / 2.899061760923799
+gamma2 = 0.8154662499199439 #6.660878194277664 #10.0
 # weight on nn output to dynamic output loss
-gamma3 = .1 #/ 47260.1495314765 #5e-6
+gamma3 = 0.0006080492581026421 #0.10271433124674645 #.1
 
 ### Define the optimizer
 optimizer = torch.optim.Adam(model.parameters())
@@ -96,7 +97,7 @@ for j in range(num_epochs):
 
         Vc_k_est = torch.tensor(Vc_all[idx[i]:idx[i+1], :], dtype=torch.float32).reshape(batch_size, 1, 8)
 
-        X_train_torch = torch.tensor(X_train[idx[i]:idx[i+1], :, :], dtype=torch.float32)
+        X_train_torch = torch.tensor(X_train[idx[i]:idx[i+1], :, :], dtype=torch.float32) / 10
         y_train_torch = torch.tensor(y_train[idx[i]:idx[i+1], :, :], dtype=torch.float32) / 1000
 
         ### TRAIN THE GENERATOR
@@ -105,7 +106,7 @@ for j in range(num_epochs):
         y_pred = model(X_train_torch)
 
         mse_loss = gamma1*loss_func(y_train_torch, y_pred[:, :, -1].reshape(-1, 1, 1))
-        Vc_k_est, physics_loss, l2, l3 = physics_loss_func(y_pred, y_train_torch, X_train_torch, Vc_k_est, gamma1, gamma2, gamma3)
+        Vc_k_est, physics_loss, l2, l3 = physics_loss_func(y_pred, y_train_torch, X_train_torch*10, Vc_k_est, gamma1, gamma2, gamma3)
         loss = mse_loss + physics_loss
         
         loss.backward()
@@ -120,6 +121,7 @@ for j in range(num_epochs):
     print(f"Epoch {j} loss: {l_tot} ({l1_tot}, {l2_tot}, {l3_tot})")
     loss_all.append(l_tot)
 
+### Plot the loss
 plt.figure()
 plt.plot(loss_all, label="Total loss")
 plt.ylabel("Total")
@@ -127,15 +129,13 @@ plt.xlabel("Epochs")
 plt.legend()
 plt.show()
 
-
-
 ### Plot the train data
 Vc_hat_train = []
 Vth_hat_train = []
 Vth_true_train = []
 for X_train, y_train in  trainloader:
 
-    y_pred_train = model(X_train).detach().numpy()*1000
+    y_pred_train = model(X_train/10).detach().numpy()*1000
 
     Vc_hat_train.append(y_pred_train[:,:,:-1].reshape(-1,8))
     Vth_hat_train.append(y_pred_train[:,:,-1].flatten())
@@ -147,14 +147,28 @@ Vc_true_train = np.array(df[Vc_colnames])[:8000,:]
 Vth_hat_train = np.array(Vth_hat_train).flatten()
 Vth_true_train =np.array(Vth_true_train).flatten()
 
-fig, ax = plt.subplots(2,1)
+fig, ax = plt.subplots(3,1)
 for i in range(8):
-    ax[0].plot(Vc_hat_train[:,i], label=f"Vc{i}")
-    ax[0].plot(Vc_true_train[:,i], 'k-')
+    if i < 4:
+        ax[0].plot(Vc_hat_train[:,i], label=f"Vc{i}")
+        ax[0].plot(Vc_true_train[:,i], 'k-')
+    else:
+        ax[1].plot(Vc_hat_train[:,i], label=f"Vc{i}")
+        ax[1].plot(Vc_true_train[:,i], 'k-')
 
-ax[1].plot(Vth_true_train, label="True")
-ax[1].plot(Vth_hat_train, label="Est")
+ax[0].set_title("Upper Arm")
+ax[0].set_xlabel("Time")
+ax[0].set_ylabel("Cap Voltage")
+ax[0].legend()
+
+ax[1].set_title("Lower Arm")
+ax[1].set_xlabel("Time")
+ax[1].set_ylabel("Cap Voltage")
 ax[1].legend()
+
+ax[2].plot(Vth_true_train, label="True")
+ax[2].plot(Vth_hat_train, label="Est")
+ax[2].legend()
 plt.show()
     
 
@@ -166,7 +180,7 @@ Vth_hat = []
 Vth_true = []
 for X_test, y_test in  valloader:
 
-    y_pred_test = model(X_test).detach().numpy()*1000
+    y_pred_test = model(X_test/10).detach().numpy()*1000
 
     Vc_hat.append(y_pred_test[:,:,:-1].reshape(-1,8))
     Vth_hat.append(y_pred_test[:,:,-1].flatten())
@@ -178,12 +192,26 @@ Vc_true = np.array(df[Vc_colnames])[-2000:,:]
 Vth_hat = np.array(Vth_hat).flatten()
 Vth_true =np.array(Vth_true).flatten()
 
-fig, ax = plt.subplots(2,1)
+fig, ax = plt.subplots(3,1)
 for i in range(8):
-    ax[0].plot(Vc_hat[:,i], label=f"Vc{i}")
-    ax[0].plot(Vc_true[:,i], 'k-')
+    if i < 4:
+        ax[0].plot(Vc_hat_train[:,i], label=f"Vc{i}")
+        ax[0].plot(Vc_true_train[:,i], 'k-')
+    else:
+        ax[1].plot(Vc_hat_train[:,i], label=f"Vc{i}")
+        ax[1].plot(Vc_true_train[:,i], 'k-')
 
-ax[1].plot(Vth_true, label="True")
-ax[1].plot(Vth_hat, label="Est")
+ax[0].set_title("Upper Arm")
+ax[0].set_xlabel("Time")
+ax[0].set_ylabel("Cap Voltage")
+ax[0].legend()
+
+ax[1].set_title("Lower Arm")
+ax[1].set_xlabel("Time")
+ax[1].set_ylabel("Cap Voltage")
 ax[1].legend()
+
+ax[2].plot(Vth_true, label="True")
+ax[2].plot(Vth_hat, label="Est")
+ax[2].legend()
 plt.show()
---
+++ main_optuna.py
@@ -0,0 +1,189 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import optuna
+
+import torch
+from torch.utils.data import DataLoader
+
+from src.utils import CustomDataset
+from src.utils import Data
+from src.utils import PINNLoss
+from src.NN import FNN, LSTM
+
+from sklearn.preprocessing import MinMaxScaler, StandardScaler
+
+torch.manual_seed(101)
+
+
+### data file
+# file = "data/combinedDataRev3Labeled.csv"
+# file = "data/combinedDataRev4SlimLabeled.csv"
+file = "data/combinedDataRev4LabeledSynthetic.csv"
+
+### create dataframe
+DataObj = Data(file)
+df = DataObj.ImportData()
+
+### define input (X) and output (y) data
+Vc_colnames = ["vc1","vc2","vc3","vc4","vc5","vc6","vc7","vc8"]
+X_colnames = ["g1upper","g2upper","g3upper", "g4upper","g5upper","g6upper","g7upper","g8upper","i1","i2"]
+y_colnames = ["vout"]
+X = np.array(df[X_colnames])[5000:15000,:]
+y = np.array(df[y_colnames])[5000:15000,:]
+
+Vc_all = np.array(df[Vc_colnames])[5000:15000,:]
+
+# ### normalize the training data
+# norm = StandardScaler().fit(X)
+# X = norm.transform(X)
+# X = DataObj.Normalize(X)
+# y = DataObj.Normalize(y)
+
+### data DD = {(X,U,D), Y}
+frac = 0.8
+X_train, y_train, X_test, y_test = DataObj.TrainTestSplit(X, y, frac=frac)
+X_train = X_train.reshape(-1, 1, 10)
+y_train = y_train.reshape(-1, 1, 1)
+X_test = X_test.reshape(-1, 1, 10)
+y_test = y_test.reshape(-1, 1, 1)
+
+### use the custom Pytorch Dataset generator to get train and test data
+train_data = CustomDataset(X_train, y_train)
+test_data = CustomDataset(X_test, y_test)
+
+### use the generated train/test data and batch size to create DataLoader objects for the train and test (validataion) sets
+batch_size = 50
+trainloader = DataLoader(train_data, batch_size=batch_size, shuffle=False)
+valloader = DataLoader(test_data, batch_size=batch_size, shuffle=False)
+
+def objective(trial, X_train, y_train, X_test, y_test):
+
+    batch_size = 200
+
+    # ### neural network parameters
+    in_dim = 10
+    hidden_dim = trial.suggest_int("hidden_layers", 64, 1024, log=True) #256
+    out_dim = 9
+
+    ### define the neural network
+    model = LSTM(in_dim, out_dim, hidden_dim)
+
+    ### training parameters
+    num_epochs = 100
+    learn_rate = trial.suggest_float("lr", 1e-6, 0.1, log=True) #0.00001
+
+    ### Define the loss function
+    physics_loss_func = PINNLoss()
+    loss_func = torch.nn.MSELoss()
+
+    # weight on nn output to true output loss
+    gamma1 = trial.suggest_float("gamma1", 1e-4, 10.0, log=True) #.1 #1.0 / 47260.1495314765 #5e-6
+    # weight on nn state output to dynamic state output loss
+    gamma2 = trial.suggest_float("gamma2", 1e-4, 10.0, log=True) #10.0 #10. #1e0 / 2.899061760923799
+    # weight on nn output to dynamic output loss
+    gamma3 = trial.suggest_float("gamma3", 1e-4, 10.0, log=True) #.1 #/ 47260.1495314765 #5e-6
+
+    ### Define the optimizer
+    optimizer = torch.optim.Adam(model.parameters(), lr=learn_rate)
+
+    ### initialize the state 
+    # Vc_k_est = torch.tensor(Vc_all[:batch_size, :], requires_grad=True, dtype=torch.float32).reshape(batch_size, 1, 8)
+
+    idx = np.linspace(0,len(X_train),int(len(X_train)/batch_size)+1).astype(int)
+    ### Train the FNN model, monitor loss
+    loss_all = []
+    for j in range(num_epochs):
+        l_tot = 0
+        l1_tot = 0
+        l2_tot = 0
+        l3_tot = 0
+        # for X_train, y_train in trainloader:
+        for i in range(len(idx)-1):
+
+            Vc_k_est = torch.tensor(Vc_all[idx[i]:idx[i+1], :], dtype=torch.float32).reshape(batch_size, 1, 8)
+
+            X_train_torch = torch.tensor(X_train[idx[i]:idx[i+1], :, :], dtype=torch.float32) / 10
+            y_train_torch = torch.tensor(y_train[idx[i]:idx[i+1], :, :], dtype=torch.float32) / 1000
+
+            optimizer.zero_grad()
+
+            y_pred = model(X_train_torch)
+
+            mse_loss = gamma1*loss_func(y_train_torch, y_pred[:, :, -1].reshape(-1, 1, 1))
+            Vc_k_est, physics_loss, l2, l3 = physics_loss_func(y_pred, y_train_torch, X_train_torch*10, Vc_k_est, gamma1, gamma2, gamma3)
+            loss = mse_loss + physics_loss
+            
+            loss.backward()
+
+            optimizer.step()
+
+            l_tot = l_tot + loss.detach()
+            l1_tot = l1_tot + mse_loss.detach()
+            l2_tot = l2_tot + l2.detach()
+            l3_tot = l3_tot + l3.detach()
+
+        # print(f"Epoch {j} loss: {l_tot} ({l1_tot}, {l2_tot}, {l3_tot})")
+        loss_all.append(l_tot)
+
+    ###Calculate training error
+    train_err = 0
+    # for X_train, y_train in  trainloader:
+    for i in range(len(idx)-1):
+
+        X_train_torch = torch.tensor(X_train[idx[i]:idx[i+1], :, :], dtype=torch.float32) / 10
+        y_train_torch = torch.tensor(y_train[idx[i]:idx[i+1], :, :], dtype=torch.float32)
+        Vc_k_est = torch.tensor(Vc_all[idx[i]:idx[i+1], :], dtype=torch.float32).reshape(batch_size, 8)
+
+        y_pred_train = model(X_train_torch/10).detach().numpy()*1000
+
+        train_err = train_err + np.mean((Vc_k_est.detach().numpy() - y_pred_train[:,:,:-1].reshape(-1,8))**2) / batch_size
+        train_err = train_err + np.mean((y_train_torch.detach().numpy().flatten() - y_pred_train[:,:,-1].flatten())**2) / batch_size
+
+
+    # ### Calculate testing error
+    # idx = np.linspace(len(X_train),len(X_train)+len(X_test),int(len(X_test)/batch_size)+1).astype(int)
+    # test_err = 0
+    # # for X_test, y_test in  valloader:
+    # for i in range(len(idx)-1):
+
+    #     X_test_torch = torch.tensor(X_test[idx[i]:idx[i+1], :, :], dtype=torch.float32) / 10
+    #     y_test_torch = torch.tensor(y_test[idx[i]:idx[i+1], :, :], dtype=torch.float32)
+    #     Vc_k_est = torch.tensor(Vc_all[idx[i]:idx[i+1], :], dtype=torch.float32).reshape(batch_size, 8)
+
+    #     y_pred_test = model(X_test_torch).detach().numpy()*1000
+
+    #     test_err = test_err + np.mean((Vc_k_est.detach().numpy() - y_pred_test[:,:,:-1].reshape(-1,8))**2) / batch_size
+    #     test_err = test_err + np.mean((y_test_torch.detach().numpy().flatten() - y_pred_test[:,:,-1].flatten())**2) / batch_size
+
+    return train_err #np.mean([train_err, test_err])
+
+
+def logging_callback(study, frozen_trial):
+    previous_best_value = study.user_attrs.get("previous_best_value", None)
+    if previous_best_value != study.best_value:
+        study.set_user_attr("previous_best_value", study.best_value)
+        print(
+            "Trial {} finished with best value: {} and parameters: {}. ".format(
+            frozen_trial.number,
+            frozen_trial.value,
+            frozen_trial.params,
+            )
+        )
+
+### Turn off optuna log notes
+optuna.logging.set_verbosity(optuna.logging.WARN)
+
+### Create the Optuna study (minimize mse)
+study = optuna.create_study(direction="minimize")
+
+### Run the Optuna study (only log trials that improve the objective)
+study.optimize(lambda trial: objective(trial, X_train, y_train, X_test, y_test), n_trials=100, callbacks=[logging_callback])
+
+print("Best trial:")
+trial = study.best_trial
+
+print(f"  Accuracy: {trial.value} %")
+
+print("  Optimal parameters: ")
+for key, value in trial.params.items():
+    print("    {}: {}".format(key, value))
